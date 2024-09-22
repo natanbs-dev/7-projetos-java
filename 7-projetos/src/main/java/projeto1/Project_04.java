fe99843efbfdd867a326e8524d9842c4bfaf529e
Natan Dev <159494554+natanbs-dev@users.noreply.github.com>
2024-09-21 22:17:43 -0300

    add: projeto 4 = FOR-PAR

---
+++ 7-projetos/src/main/java/projeto1/Project_04.java
@@ -0,0 +1,11 @@
+package projeto1;
+
+//faça um loop que printe na tela somente os números pares até 50
+public class Project_04 {
+    public static void main(String[] args) {
+
+        for(int i = 0; i <=100; i+=2) {
+            System.out.println(i + " ");
+        }
+    }
+}
