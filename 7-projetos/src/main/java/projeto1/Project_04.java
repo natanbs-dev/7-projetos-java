88b02965a3b28e6f4f68c32f99cdcbd9bd3504e3
Natan Dev <159494554+natanbs-dev@users.noreply.github.com>
2024-09-22 22:47:24 -0300

    add:FOR com números primos

--- 7-projetos/src/main/java/projeto1/Project_04.java
+++ 7-projetos/src/main/java/projeto1/Project_04.java
@@ -1,11 +1,23 @@
 package projeto1;
 
 //faça um loop que printe na tela somente os números pares até 50
+//public class Project_04 {
+//    public static void main(String[] args) {
+//
+//        for(int i = 0; i <=100; i+=2) {
+//            System.out.println(i + " ");
+//        }
+//    }
+//}
+
+// faça um loop somente printando os valores primos nessa lop
 public class Project_04 {
     public static void main(String[] args) {
 
-        for(int i = 0; i <=100; i+=2) {
-            System.out.println(i + " ");
+        for(int i = 0; i <=50; i++) {
+            if (i % 2 != 0)
+                System.out.print(i + " ");
+
         }
     }
 }
